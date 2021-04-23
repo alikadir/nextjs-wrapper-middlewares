ca4119e85730209ddf8fa300fbb7008e4f5d0ddd
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-04-23 13:06:18 +0300

    added awesome response body reader for json

--- test/cacheService.test.js
+++ test/cacheService.test.js
@@ -1,10 +1,19 @@
+import redis from 'redis';
 import { getCacheKey, setCacheKey } from '../lib/cacheService.js';
 
 describe('cacheService', () => {
+  beforeAll(() => {
+    jest.mock('redis');
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
   const testKey = 'test-key';
   const testValue = `sample test value - ${new Date().toString()}`;
 
   test('set key', async (done) => {
+    redis.createClient().get.mock;
     await setCacheKey(testKey, testValue, 75);
     done();
   });

a41669877c09ffc269bca5e1909815edd7a0d7af
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-04-23 15:46:43 +0300

    log and cache wrapper

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,8 @@
-import { cacheResponse } from '../../lib/cacheService.js';
-import { loggerRequest } from '../../lib/logService.js';
+import { cacheResponseWrapper } from '../../lib/cacheService.js';
+import { loggerRequestResponseWrapper } from '../../lib/logService.js';
 
 const handler = async (req, res) => {
-  if (Math.floor(Math.random() * 100) % 2 === 0)
-    res.status(200).cacheJson({ name: 'John Doe', query: req.query, date: new Date().toLocaleString() }, 10);
-  else res.json({ name: 'John Doe' });
+  res.status(200).json({ name: 'John Doe', query: req.query, date: new Date().toLocaleString() }, 10);
 };
 
-export default loggerRequest(cacheResponse(handler));
+export default loggerRequestResponseWrapper(cacheResponseWrapper(handler));

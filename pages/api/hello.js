088d17a2b012f1044c79bd5ab319e4879462ff1b
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-04-24 16:03:01 +0300

    log and cache wrapper functions as middleware for nextjs

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ import { cacheResponseWrapper } from '../../lib/cacheService.js';
 import { loggerRequestResponseWrapper } from '../../lib/logService.js';
 
 const handler = async (req, res) => {
-  res.status(200).json({ name: 'John Doe', query: req.query, date: new Date().toLocaleString() }, 10);
+  res.status(201).json({ name: 'John Doe', query: req.query, date: new Date().toLocaleString() }, 10);
 };
 
 export default loggerRequestResponseWrapper(cacheResponseWrapper(handler));
